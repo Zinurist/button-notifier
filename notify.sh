6367d20267476d59607030d40cee139d002b5c33
Nicolas Lindae <nicolas@lindae.de>
2023-03-12 09:05:07 +0100

    add discord webhook

--- notify.sh
+++ notify.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-CHAT_ID=""
-BOT_TOKEN=""
-TEXT="bot+notification"
-curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage?chat_id=$CHAT_ID&text=$TEXT"
+
+TELEGRAM_CHAT_ID=""
+TELEGRAM_BOT_TOKEN=""
+
+DISCORD_WEBHOOK=""
+
+TEXT=""
+
+curl -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage?chat_id=$TELEGRAM_CHAT_ID&text=$TEXT"
 echo ""
+curl -H "Content-Type: application/json" -d '{"username": "mayabot", "content": "'"$TEXT"'"}' "$DISCORD_WEBHOOK"
